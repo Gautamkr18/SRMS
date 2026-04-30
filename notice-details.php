cdef03c16df207ef5d9fe45d276a08988b633aa8
Gautamkr18 <gautamgupta1315@gmail.com>
2026-05-01 01:04:12 +0530

    Updated copyright to @GG.Tech

--- notice-details.php
+++ notice-details.php
@@ -68,7 +68,7 @@ foreach($results as $result)
 
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Student Result Management System <?php echo date('Y');?></p></div>
+            <div class="container"><p class="m-0 text-center text-white">All CopyRight @GG.Tech</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
